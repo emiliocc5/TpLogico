b6c3bcbe5d76212a5ce5cc5ba23ff269f0787977
Emilio Caccia <emiliocaccia@live.com.ar>
2019-06-14 20:27:36 -0300

    Create tp.pl

---
+++ tp.pl
@@ -0,0 +1,35 @@
+partido(frank,rojo).
+partido(claire,rojo).
+partido(garrett,azul).
+partido(jackie,amarillo).
+partido(linda,azul).
+partido(catherine,rojo).
+partido(seth,amarillo).
+partido(heather,amarillo).
+edad(fran,50).
+edad(claire,52).
+edad(garrett,64).
+edad(peter,26).
+edad(jackie,38).
+edad(linda,30).
+edad(catherine,59).
+anioNacio(heather,1969).
+
+sePostula(azul, buenosaires, chaco, tierradelfuego, sanluis,neuquen).
+sePostula(rojo, buenosaires, santafe, tierradelfuego, cordoba,chubut,sanluis).
+sePostula(azul, buenosaires, chaco, tierradelfuego, sanluis,neuquen).
+
+cantidadHabitantes(buenosaires, ).
+cantidadHabitantes(chaco, ).
+cantidadHabitantes(buenosaires, ).
+cantidadHabitantes(buenosaires, ).
+cantidadHabitantes(buenosaires, ).
+cantidadHabitantes(buenosaires, ).
+cantidadHabitantes(buenosaires, ).
+cantidadHabitantes(buenosaires, ).
+cantidadHabitantes(buenosaires, ).
+cantidadHabitantes(buenosaires, ).
+cantidadHabitantes(buenosaires, ).
+cantidadHabitantes(buenosaires, ).
+cantidadHabitantes(buenosaires, ).
+cantidadHabitantes(buenosaires, ).
